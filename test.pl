b1e28e4534dec13617a0612cda3bb3ea22a5965a
Somesh <somesh.malviya@gmail.com>
2016-10-11 16:21:50 +0530

    adding to test revert feature

---
+++ test.pl
@@ -0,0 +1,26 @@
+use warnings;
+use strict;
+my @inp = qw(5th
+        ascot
+        ate
+        carrot
+        coast
+        coats
+        cots
+        Dorian
+        eat
+        halls
+        headskin
+        inroad
+        nakedish
+        ordain
+        Ronald's
+        shall
+        sinkhead
+        tacos
+        tea);
+
+my $arr = (1);
+push $arr, 2;
+
+print join(" ", @arr);
